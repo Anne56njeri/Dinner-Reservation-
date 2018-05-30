b31e06201addfd159b96c5a037f7ffa8703cce57
MaryAnne45 <mary.jereh@gmail.com>
2018-05-30 17:16:39 +0300

    add a custom signup form to save both user and profile information onclick

---
+++ dinner_project/Reserve/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from .models import Profile
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+#create your forms here
+
+class SignUpForm(UserCreationForm):
+    '''
+    A signup form that picks both user information and the profile information
+    '''
+
+    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
+    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    username=forms.CharField(max_length=30, required=False, help_text='Optional.')
+    sex = forms.ChoiceField(choices=(('Male','Male'),('Female','Female')))
+    profile_image=forms.ImageField()
+    class Meta:
+        model = User
+        fields = ('first_name','last_name','email','username','profile_image','sex','password1', 'password2')
--- dinner_project/Reserve/views.py
+++ dinner_project/Reserve/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from .forms import SignUpForm
+from django.contrib.auth.decorators import login_required
+from .models import Profile
 # Create your views here.
+def signup(request):
+    '''
+    when the user saves the form this function is called and saves both to the user and profile model
+    '''
+    if request.method == 'POST':
+        form=SignUpForm(request.POST,request.FILES)
+        if form.is_valid():
+            user=form.save()
+            user.refresh_from_db()
+            user.profile.username=form.cleaned_data.get('username')
+            user.profile.profile_image=form.cleaned_data.get('profile_image')
+            user.profile.sex=form.cleaned_data.get('sex')
+            user.save()
+            raw_password=form.cleaned_data.get('password1')
+            user=authenticate(username=user.username,password=raw_password)
+    else:
+        form=SignUpForm()
+    return render(request,'signup.html',{"form":form})
+
 def welcome(request):
     title="Welcome to reserve"
     return render(request,'Hotel/welcome.html',{"title":title})
